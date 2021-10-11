98279ae6542e5c0b5319d36782cc983df5536071
Jacob Wharrie <jacobwharrie@shaw.ca>
2021-10-10 23:58:06 -0700

    minor syntax fix in private-api

--- private-api/src/nutrients/nutrients.service.ts
+++ private-api/src/nutrients/nutrients.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateNutrientDto } from './dto/create-nutrient.dto';
 import { UpdateNutrientDto } from './dto/update-nutrient.dto';
-import { Nutrient } from './entities/nutrient.entity'
+import { Nutrient } from './entities/nutrient.entity';
 
 @Injectable()
 export class NutrientsService {
